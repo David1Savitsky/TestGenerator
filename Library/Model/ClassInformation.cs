f8c8e71f47595185a23f4b7dda8301e1c265a041
David1Savitsky <omgprogress24@gmail.com>
2022-11-26 01:19:06 +0300

    Create model classes

---
+++ Library/Model/ClassInformation.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Library.Model
+{
+    public class ClassInformation
+    {
+        public string ClassName { get; }
+        public MemberDeclarationSyntax TestClassDeclarationSyntax { get; }
+
+        public ClassInformation(string className, MemberDeclarationSyntax testClassDeclarationSyntax)
+        {
+            ClassName = className;
+            TestClassDeclarationSyntax = testClassDeclarationSyntax;
+        }
+    }
+}
