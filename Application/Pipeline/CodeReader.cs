78db5b35472d16d490964fffa8aed3aa092238d9
David1Savitsky <omgprogress24@gmail.com>
2022-11-26 01:07:17 +0300

    Add pipeline logic

---
+++ Application/Pipeline/CodeReader.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Application;
+
+public class CodeReader
+{
+    public async Task<string> Read(string fileName)
+    {
+        using var reader = File.OpenText(fileName);
+        return await reader.ReadToEndAsync();  
+    }
+}
---
+++ Application/Pipeline/CodeWriter.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Application;
+
+public class CodeWriter
+{
+    public async Task Write(string path, string fileContent)
+    {
+        using var writer = new StreamWriter(path);
+        await writer.WriteAsync(fileContent);    
+    }
+}
