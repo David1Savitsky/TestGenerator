812549e29fc55fba37d18b2323e5a8b26f807219
David1Savitsky <omgprogress24@gmail.com>
2022-11-26 00:59:37 +0300

    Create main method

---
+++ Application/Main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    internal static class TestGeneratorScript
+    {
+        public static async Task Main(string[] args)
+        {
+            var correctInputFiles = new List<string>();
+            correctInputFiles.Add("X:\\Programming\\University\\test\\input2.txt");
+            var outputDirectory = "X:\\Programming\\University\\test\\output";
+            var readingTaskRestriction = 20;
+            var processTaskRestriction = 20;
+            var writeTaskRestriction = 20;
+            var testGeneratorService = new PipelineService(readingTaskRestriction, processTaskRestriction,
+                writeTaskRestriction, outputDirectory);
+            await testGeneratorService.Generate(correctInputFiles);
+        }
+    }
+}
